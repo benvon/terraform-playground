a42f77f0b768deab42cd7bb3932d3a317973255f
Ben Vaughan <ben@benvon.net>
2023-08-14 14:23:23 +0000

    reversy world

---
+++ modules/another_world/main.tf
@@ -0,0 +1,6 @@
+locals {
+    reversed = reverse(var.input_string)
+    reversed_lower = lower(local.reversed)
+    reversed_upper = upper(local.reversed)
+    reversed_squashed = replace(local.reversed, " ", "")
+}
---
+++ modules/another_world/variables.tf
@@ -0,0 +1,4 @@
+variable "input_string" {
+    type = string
+    description = "Input string"
+}
