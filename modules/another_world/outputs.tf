3d11db89220cda46dcf4fd9ab6d179c257c7ef50
Ben Vaughan <ben@benvon.net>
2023-08-14 09:23:54 -0500

    reversy world (#1)

---
+++ modules/another_world/outputs.tf
@@ -0,0 +1,15 @@
+output "another_world" {
+    value = local.reversed
+}
+
+output "another_world_lower" {
+    value = local.reversed_lower
+}
+
+output "another_world_upper" {
+    value = local.reversed_upper
+}
+
+output "another_world_squashed" {
+    value = local.reversed_squashed
+}
